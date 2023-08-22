d9c5a07e153283d2126162bcd08328697920429d
JosepFoster <142855814+JosepFoster@users.noreply.github.com>
2023-08-22 17:37:48 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joe Foster
+ *         jfoster23@husker.unl.edu
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joe Foster\n");
+
+  return 0;
+}
